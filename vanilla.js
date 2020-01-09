26a869a85568b11de0ef7323f4e99eb351010901
Julia M <julia@halfhelix.com>
2020-01-09 13:52:17 +0200

    first commit

---
+++ vanilla.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// const myName = 'Sasha';
+
+// let sym = Symbol();
+
+// console.log(sym);
+
+// //alert('hello world!');
+
+// confirm('how you do');
+
+let incr = 10,
+  decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+let string = 'string';
